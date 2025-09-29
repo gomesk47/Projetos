70c2e78ac5fec8cc3990444b97cfc49fbf76a792
gomesk47 <gomesk47@icloud.com>
2025-09-29 14:39:59 -0300

    Delete Estruturas de dados em P.py

--- Estruturas de dados em P.py
+++
@@ -1,6 +0,0 @@
-texto = "Explorando a diversidade de linguagens de programação com python."
-
-print(f"Tamanho do texto = {len(texto)}")
-print(f"Python in texto = {"python" in texto}")  
-print(f"Quantidade de e no texto = {texto.count('e')}")
-print(f"As cinco primeiras letras são: {texto[: 5]}")
