790126764b3460ed656b1e4e6345360120e2df6a
gomesk47 <gomesk47@icloud.com>
2025-09-29 14:40:34 -0300

    Delete Estruturas de dados em Pyt.py

--- Estruturas de dados em Pyt.py
+++
@@ -1,6 +0,0 @@
-#Função.lower e listcomp
-
-linguagens = ['Python', 'Java', 'JavaScript', 'C', 'C#', 'C++', 'Swift', 'Cobalt', 'Kotlin']
-print("Antes da listcomp = ", linguagens)
-linguagens = [item.lower() for item in linguagens]
-print("\nDepos da listcomp = ", linguagens)
