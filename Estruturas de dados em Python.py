dc3c105509259ea27eaf924f26d0cc9a300c184d
gomesk47 <gomesk47@icloud.com>
2025-09-29 14:40:54 -0300

    Delete Estruturas de dados em Python.py

--- Estruturas de dados em Python.py
+++
@@ -1,6 +0,0 @@
-#A ORDEM IMPORTA
-#TUPLAS E UTILIDADES
-vogais = ('a', 'e', 'i', 'o', 'u')
-print(f"Tipo de objeto vogais = (type(vogais))")
-for p, x in enumerate(vogais):
-    print(f"Posição = {p}, valor = {x}")
