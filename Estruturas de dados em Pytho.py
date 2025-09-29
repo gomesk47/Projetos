c00c3dd74155c09c51d2cd04533bf670c0ff2048
gomesk47 <gomesk47@icloud.com>
2025-09-29 14:40:40 -0300

    Delete Estruturas de dados em Pytho.py

--- Estruturas de dados em Pytho.py
+++
@@ -1,7 +0,0 @@
-# Função filter;
-# Filtra os elementos de uma sequencia com base em uma função de teste (retorna true or false)
-# filter(funcao_teste, sequencia)
-
-numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-numeros_pares = list(filter(lambda x: x % 2 == 0, numeros))
-print(numeros_pares)
